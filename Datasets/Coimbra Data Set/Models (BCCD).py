f04304d4257d606e8b5e78f95e27b78cf6f9f273
Arnab Roy <arnabaroy@outlook.com>
2021-03-28 18:01:46 +0530

    + Addtional Datasets For Testing Model

--- Datasets/Coimbra Data Set/Models (BCCD).py
+++ Datasets/Coimbra Data Set/Models (BCCD).py
@@ -6,7 +6,6 @@ import matplotlib.pyplot as plt
 
 from sklearn.decomposition import PCA
 from sklearn.preprocessing import StandardScaler
-from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 
 ################################################### Importing data ###################################################
@@ -25,16 +24,17 @@ del dropped_features
 ########################################## Comparison of traditional models ###########################################
 
 from sklearn.svm import SVC
-from sklearn.metrics import accuracy_score
 from sklearn.naive_bayes import GaussianNB
+from sklearn.metrics import accuracy_score
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
 
 test_set_concentration = [10, 15, 20, 25, 30, 35, 40, 45, 50]
 final_acc = pandas.DataFrame()
   
-for j in range(0, 100) :
+for j in range(0, 1) :
     
     interim_acc = pandas.DataFrame()
     
@@ -59,36 +59,38 @@ for j in range(0, 100) :
         del X_Train, X_Test, Y_Train, Y_Test, Y_Prediction
     
     final_acc = pandas.concat([final_acc, interim_acc], axis = 1)
-    del i, interim_acc
+    del i, interim_acc # Delete Temporary Variables
 
-del j
+del j # Delete Temporary Variables
 
 final_acc['Median'] = final_acc.median(axis = 1)           
 final_acc.to_csv('Validation Accuracy.csv')
 
-###################################################### ANN Model ######################################################
+################################################# ANN Model (Single Layer) ###########################################
 
 import keras
 import tensorflow
+import matplotlib.pyplot as plt
 
 from keras import callbacks
 from keras.layers import Dense
 from keras.optimizers import SGD
 from keras.models import Sequential
+from sklearn.model_selection import train_test_split
 
-test_set_concentration = [10]
+test_set_concentration = [50]
 accuracy_history = pandas.DataFrame()
 
 for i in test_set_concentration :
     
     es = callbacks.EarlyStopping(monitor = 'val_loss', mode = 'min', patience = 10, restore_best_weights = False, verbose = 1)
     
-    for j in range(0, 2) :
+    for j in range(0, 1) :
         
         X_Train, X_Test, Y_Train, Y_Test = train_test_split(X, Y, test_size = i/100, shuffle = True, random_state = 0) # Test-Train Split
         
         classifier = Sequential()
-        classifier.add(Dense(8, kernel_initializer = 'uniform', activation = 'relu', input_dim = 16)) # First hidden layer
+        classifier.add(Dense(5, kernel_initializer = 'uniform', activation = 'relu', input_dim = 9)) # First hidden layer
         classifier.add(Dense(1, kernel_initializer = 'uniform', activation = 'sigmoid')) # Output layer
         classifier.compile(optimizer = 'SGD', loss = 'binary_crossentropy', metrics = 'accuracy')
         
@@ -102,22 +104,27 @@ for i in test_set_concentration :
         
         # Plot
     
-        # training_loss = model_data.history['loss']
-        # test_loss = model_data.history['val_loss']
-        # epoch_count = range(1, len(training_loss) + 1)
+        training_loss = model_data.history['loss']
+        test_loss = model_data.history['val_loss']
+        epoch_count = range(1, len(training_loss) + 1)
     
-        # plt.plot(epoch_count, training_loss, '--', color = 'red')
-        # plt.plot(epoch_count, test_loss, '--', color = 'blue')
-        # plt.legend(['Training Loss', 'Test Loss'])
-        # plt.xlabel('Number of Epoch')
-        # plt.ylabel('Validation Loss')
-        # plt.show()
+        plt.plot(epoch_count, training_loss, '--', color = 'red')
+        plt.plot(epoch_count, test_loss, '--', color = 'blue')
+        plt.legend(['Training Loss', 'Test Loss'])
+        plt.xlabel('Number of Epoch')
+        plt.ylabel('Validation Loss')
+        plt.show()
         
         # Deleting temporary variables
         
         del X_Test, X_Train, Y_Test, Y_Train, classifier, model_data
         del temporary_history, very_temporary_history
-        # del training_loss, test_loss, epoch_count
+        del training_loss, test_loss, epoch_count
+      
+    # Print the result in a CSV File
+    
+    accuracy_history['Median'] = accuracy_history.median(axis = 1)
+    accuracy_history.to_csv('Result - {}%.csv'.format(i))
 
 ##################################### Performance Evaluation with AUC-ROC Method ######################################
 
---
+++ Datasets/Coimbra Data Set/Visualization (BCCD).py
@@ -0,0 +1,104 @@
+##################################### Importing dataset and Preliminary Analysis ######################################
+
+import numpy
+import pandas
+import seaborn
+
+### Import Dataset and Split
+
+dataset = pandas.read_csv('dataR2.csv')
+dataset_info = pandas.concat([dataset.dtypes, dataset.nunique(dropna = False).sort_values(ascending = False), dataset.isnull().sum().sort_values(ascending = False), (100*dataset.isnull().sum()/dataset.isnull().count()).sort_values(ascending = False)], axis = 1, keys = ['Type', 'Unique Values', 'Null Values', 'Null Percentage']) # Null Value Check
+
+### Line plot of mean of individual features ###
+
+ax = seaborn.lineplot(data = X.mean())
+ax.set(xticklabels = [])
+ax.set(xlabel = 'Features')
+
+del ax # Delete Temporary Variables
+
+### Understanding the distribution of the target variable among classes ###
+
+P, H = Y.value_counts()
+print('Number of Patients : ', P)
+print('Number of Non-Patients : ', H)
+ax = seaborn.countplot(Y, label = 'Count')
+
+del ax, P, H # Delete Temporary Variables
+
+############################################ Outlier Treatment and Normalization #####################################
+
+dataset.columns = ['Age', 'BMI', 'Glucose', 'Insulin', 'HOMA', 'Leptin', 'Adiponectin', 'Resistin', 'MCP1', 'Classification'] # Reformat Column Names
+# dataset.boxplot(column = 'Age', vert = True) # Box and Whisker Plot of feature
+
+for i in dataset.columns :
+    
+    if i != 'Age' and i != 'BMI' and i != 'Classification' :
+    
+        Q1 = dataset[i].quantile(0.25) # First Quartile
+        Q3 = dataset[i].quantile(0.75) # Third Quartile
+        IQR = Q3 - Q1 # Interquartile Range
+        
+        # Filtering Values between (Q1-1.5IQR) and (Q3+1.5IQR)
+        dataset.query('(@Q1 - 1.5 * @IQR) <= {} <= (@Q3 + 1.5 * @IQR)'.format(i), inplace = True)
+        
+        del Q1, Q3, IQR # Delete Temporary Variables
+
+del i # Delete Temporary Variables
+
+X = dataset.drop(['Classification'], axis = 1) # Independent variables
+Y = dataset.Classification # Target variables
+
+### Normalization of features ###
+X = (X - X.min()) / (X.max() - X.min())
+
+###################################################### Feature Importance #############################################
+
+from sklearn.datasets import make_classification
+from sklearn.ensemble import RandomForestClassifier
+
+features = {}
+model = RandomForestClassifier()
+model.fit(X, Y)
+
+for a, b in zip(X.columns, model.feature_importances_):
+    features[a] = b 
+
+importances = pandas.DataFrame.from_dict(features, orient = 'index').rename(columns = {0: 'Importance'})
+importances.sort_values(by = 'Importance').plot(kind = 'barh')
+
+# Delete Temporary Variables
+
+del a, b
+del features, importances, model
+
+######################################### Swarm Plot of Insignificant Features ########################################
+
+columns = ['HOMA', 'Insulin']
+
+features = pandas.concat([Y, X[columns]], axis = 1)
+features = pandas.melt(features, id_vars = 'Classification', var_name = 'features', value_name = 'value')
+
+plot = seaborn.set(style = 'whitegrid', palette = 'muted')
+plot = seaborn.swarmplot(x = 'features', y = 'value', hue = 'Classification', data = features)
+
+# Delete Temporary Variables
+
+del columns, features, plot
+
+############################################ Pearson Correlation Matrix ###############################################
+
+mask = numpy.zeros_like(X.corr())
+mask[numpy.triu_indices_from(mask)] = True
+
+with seaborn.axes_style("white") :
+    
+    f, ax = plt.subplots(figsize = (18, 18))
+    ax = seaborn.heatmap(X.corr(), mask = mask, annot = True, linewidths = .5, fmt = '.1f', square = True)
+    
+############################################### Drop Insignificant Features ###########################################
+
+insignificant_features = ['Insulin']
+X = X.drop(insignificant_features, axis = 1)
+
+del insignificant_features # Delete Temporary Variables
