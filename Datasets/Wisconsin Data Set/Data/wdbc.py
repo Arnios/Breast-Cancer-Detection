232d9900ca49397fa41723571c579a435a99e132
Arnab Roy <arnabaroy@outlook.com>
2021-04-04 19:04:08 +0530

    + PCA For WDBC

--- Datasets/Wisconsin Data Set/Data/wdbc.py
+++ Datasets/Wisconsin Data Set/Data/wdbc.py
@@ -20,25 +20,140 @@ import numpy
 import pandas
 import seaborn
 
-dataset = pandas.read_csv('wdbc.data', delimiter = ",", header = None, names = ['ID number', 'Diagnosis', 'radius mean', 'texture mean', 'perimeter mean', 'area mean', 'smoothness mean', 'compactness mean', 'concavity mean', 'concave points mean', 'symmetry mean', 'fractal dimension mean', 'radius se', 'texture se', 'perimeter se', 'area se', 'smoothness se', 'compactness se', 'concavity se', 'concave points se', 'symmetry se', 'fractal dimension se', 'radius worst', 'texture worst', 'perimeter worst', 'area worst', 'smoothness worst', 'compactness worst', 'concavity worst', 'concave points worst', 'symmetry worst', 'fractal dimension worst'])
+dataset = pandas.read_csv('wdbc.data', delimiter = ",", header = None, names = ['ID_number', 'Diagnosis', 'radius_mean', 'texture_mean', 'perimeter_mean', 'area_mean', 'smoothness_mean', 'compactness_mean', 'concavity_mean', 'concave_points_mean', 'symmetry_mean', 'fractal_dimension_mean', 'radius_se', 'texture_se', 'perimeter_se', 'area_se', 'smoothness_se', 'compactness_se', 'concavity_se', 'concave_points_se', 'symmetry_se', 'fractal_dimension_se', 'radius_worst', 'texture_worst', 'perimeter_worst', 'area_worst', 'smoothness_worst', 'compactness_worst', 'concavity_worst', 'concave_points_worst', 'symmetry_worst', 'fractal_dimension_worst'])
 information = pandas.concat([dataset.dtypes, dataset.nunique(dropna = False).sort_values(ascending = False), dataset.isnull().sum().sort_values(ascending = False), (100*dataset.isnull().sum()/dataset.isnull().count()).sort_values(ascending = False)], axis = 1, keys = ['Type', 'Unique Values', 'Null Values', 'Null Percentage']) # Null Value Check
 
-X = dataset.drop(['ID number', 'Diagnosis'], axis = 1) # Independent variables
-Y = dataset['Diagnosis'] # Dependant variables
+################################################# Understanding the data #############################################
+
+import matplotlib.pyplot as plt
+
+### Target variable distribution ###
 
 ax1 = seaborn.countplot(Y, label = 'Count')
 B, M = Y.value_counts()
 print('Number of Benign: ', B)
 print('Number of Malignant : ', M)
 
+### Feature mean distribution ###
+
 ax2 = seaborn.lineplot(data = X.mean()) # Line plot of mean of individual features
 ax2.set(xticklabels = [])
 ax2.set(xlabel = 'Features')
 
-# Delete Temporary Variables
+### Creating a 5X6 Boxplot matrix of the independent variables ###
+
+figure = plt.figure()
+
+for feature, num in zip(X.columns, range(1, 31)) :
+    
+    ax = figure.add_subplot(5, 6, num)
+    ax.boxplot(X[feature])
+    ax.set_title(feature, size = 5, pad = -0.5)
+    ax.set_xticks([])
+    ax.set_yticks([])
+
+plt.show()
+
+### Deleting Temporary Variables ###
 
 del ax1, ax2
 del B, M
+del figure, feature, num, ax, plt
+
+#################################################### Outlier Elimination #############################################
+
+for feature in dataset :
+    
+    if feature != 'ID_number' and feature != 'Diagnosis' :
+    
+        Q1 = dataset[feature].quantile(0.25) # First Quartile
+        Q3 = dataset[feature].quantile(0.75) # Third Quartile
+        IQR = Q3 - Q1 # Interquartile Range
+        
+        # Filtering Values between (Q1-1.5IQR) and (Q3+1.5IQR)
+        dataset.query('(@Q1 - 1.5*@IQR) <= {} <= (@Q3 + 1.5*@IQR)'.format(feature), inplace = True)
+        
+        del Q1, Q3, IQR # Delete Temporary Variables
+
+del feature # Delete Temporary Variables
+
+####################################################### Split Dataset ################################################
+
+X = dataset.drop(['ID_number', 'Diagnosis'], axis = 1) # Independent variables
+Y = dataset['Diagnosis'] # Dependant variables
+
+### Normalization ###
+
+X = (X - X.min()) / (X.max() - X.min())
+# X = StandardScaler().fit_transform(X) # Feature scaling
+# Y = LabelEncoder().fit_transform(Y) # Enconding the categorical dependant variable
+
+################################################ Principal Component Analysis ########################################
+
+import numpy
+import pandas
+import seaborn
+import matplotlib.pyplot as plt
+
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+
+test_set_concentration = [10, 15, 20, 25, 30, 35, 40, 45, 50]
+pca_scores = pandas.DataFrame()
+
+### Conduct PCA on varying test case concentration ###
+
+for i in test_set_concentration :
+
+    X_Train, X_Test, Y_Train, Y_Test = train_test_split(X, Y, test_size = i/100, random_state = 1) # Split data into train and test sets
+    
+    ### Min-Max Normalization of features in Train-Test Set ###
+    
+    X_Train = (X_Train - X_Train.min()) / (X_Train.max() - X_Train.min())
+    X_Test = (X_Test - X_Test.min()) / (X_Test.max() - X_Test.min())
+    
+    # X_Train = StandardScaler().fit_transform(X_Train)
+    # X_Test = StandardScaler().fit_transform(X_Test)
+    
+    ### Principal Component Analysis ###
+    
+    pca = PCA(n_components = None)
+    
+    X_Train = pca.fit_transform(X_Train) 
+    X_Test = pca.fit_transform(X_Test)
+    
+    # explained_variance = pca.explained_variance_ratio_
+    
+    ### Obtain PCA Score of Individual Features ###
+    
+    features = {}
+    
+    for a, b in zip(X.columns, pca.explained_variance_ratio_) :
+        features[a] = b
+    
+    importances = pandas.DataFrame.from_dict(features, orient = 'index').rename(columns = {0: '{}%'.format(i)})
+    pca_scores = pandas.concat([pca_scores, importances], axis = 1)
+
+    ### Plot ###
+    
+    # plt.figure(1, figsize = (18, 18))
+    # plt.plot(pca.explained_variance_ratio_, linewidth = 2)
+    # plt.axis('tight')
+    # plt.xlabel('Number of components')
+    # plt.ylabel('Explained Variance Ratio')
+    
+### Delete Temporary Variables ###
+
+del a, b, i
+del X_Train, X_Test, Y_Train, Y_Test, features
+del pca, importances, test_set_concentration
+
+### Calculate the median importance of each feature ###
+
+pca_scores['Mean'] = pca_scores.mean(axis = 1)
+pca_scores['Mean'].sort_values(ascending = False).plot(kind = 'bar', rot = 90)
 
 ######################################################### ANOVA F-Test ###############################################
 
@@ -60,7 +175,7 @@ def select_features(X_Train, Y_Train, X_Test) :
     X_Test_fs = fs.transform(X_Test) # Transform test input data
     return X_Train_fs, X_Test_fs, fs
 
-### Conduct ANOVA F-Test for varying test case concentration
+### Conduct ANOVA F-Test for varying test case concentration ###
 
 for i in test_set_concentration :
 
@@ -73,7 +188,7 @@ for i in test_set_concentration :
     for a, b in zip(X.columns, fs.scores_) :
         features[a] = b
     
-    importances = pandas.DataFrame.from_dict(features, orient = 'index').rename(columns = {0: 'Importance'})
+    importances = pandas.DataFrame.from_dict(features, orient = 'index').rename(columns = {0: '{}%'.format(i)})
     anova_fs_scores = pandas.concat([anova_fs_scores, importances], axis = 1)
 
 ### Delete Temporary Variables ###
@@ -83,47 +198,14 @@ del X_Train, X_Test, Y_Train, Y_Test, X_Train_fs, X_Test_fs, features
 
 ### Calculate the median importance of each feature ###
 
-anova_fs_scores['Median'] = anova_fs_scores.median(axis = 1)
-anova_fs_scores['Median'].sort_values(ascending = False).plot(kind = 'bar', rot = 90)
+anova_fs_scores['Mean'] = anova_fs_scores.mean(axis = 1)
+anova_fs_scores['Mean'].sort_values(ascending = False).plot(kind = 'bar', rot = 90)
 
-############################################ Outlier Treatment and Normalization #####################################
+############################################### Drop Insignificant Features ###########################################
 
-import matplotlib.pyplot as plt
-
-# Creating a 5X6 Boxplot matrix of the independent variables
-
-figure = plt.figure()
-
-for feature, num in zip(X.columns, range(1, 31)) :
-    
-    ax = figure.add_subplot(5, 6, num)
-    ax.boxplot(X[feature])
-    ax.set_title(feature, size = 5, pad = -0.5)
-    ax.set_xticks([])
-    ax.set_yticks([])
-
-plt.show()
-
-del figure, feature, num, ax, plt # Delete Temporary Variables
-
-### Elimination of outliers ###
-
-for feature in X :
-    
-    Q1 = X[feature].quantile(0.25) # First Quartile
-    Q3 = X[feature].quantile(0.75) # Third Quartile
-    IQR = Q3 - Q1 # Interquartile Range
-    
-    # Filtering Values between (Q1-1.5IQR) and (Q3+1.5IQR)
-    X.query('(@Q1 - 1.5*@IQR) <= {} <= (@Q3 + 1.5*@IQR)'.format(feature), inplace = True)
-            
-    del Q1, Q3, IQR # Delete Temporary Variables
-
-del feature # Delete Temporary Variables
-
-### Min-Max Normalization of features ###
-
-X = (X - X.min()) / (X.max() - X.min())
+insignificant_features = ['smoothness_mean', 'compactness_mean', 'symmetry_mean', 'fractal_dimension_mean', 'radius_se', 'texture_se', 'smoothness_se', 'compactness_se', 'concavity_se', 'concave points_se', 'symmetry_se', 'fractal_dimension_se', 'symmetry_worst', 'fractal_dimension_worst']
+X = X.drop(insignificant_features, axis = 1)
+dataset = pandas.concat([Y, X], axis = 1)
 
 ###################################################### Feature Importance #############################################
 
@@ -169,7 +251,7 @@ else :
 del prob, alpha, table
 del stat, p, dof, expected, critical
 
-### Random Forest ###
+############################################ Feature Importance via Random Forest ####################################
 
 from sklearn.datasets import make_classification
 from sklearn.ensemble import RandomForestClassifier
@@ -185,7 +267,7 @@ importances = pandas.DataFrame.from_dict(features, orient = 'index').rename(colu
 
 importances.sort_values(by = 'Importance').plot(kind = 'bar', rot = 90)
 
-######################################### Swarm Plot of Insignificant Features ########################################
+################################################# Swarm Plot Analysis ################################################
 
 # columns = ['smoothness_mean', 'compactness_mean', 'symmetry_mean']
 # columns = ['fractal_dimension_mean', 'radius_se', 'texture_se']
@@ -198,12 +280,6 @@ features = pandas.melt(features, id_vars = 'diagnosis', var_name = 'features', v
 
 plot = seaborn.set(style = 'whitegrid', palette = 'muted')
 plot = seaborn.swarmplot(x = 'features', y = 'value', hue = 'diagnosis', data = features)
-
-############################################### Drop Insignificant Features ###########################################
-
-insignificant_features = ['smoothness_mean', 'compactness_mean', 'symmetry_mean', 'fractal_dimension_mean', 'radius_se', 'texture_se', 'smoothness_se', 'compactness_se', 'concavity_se', 'concave points_se', 'symmetry_se', 'fractal_dimension_se', 'symmetry_worst', 'fractal_dimension_worst']
-X = X.drop(insignificant_features, axis = 1)
-dataset = pandas.concat([Y, X], axis = 1)
 
 ############################################ Pearson Correlation Matrix ###############################################
 
